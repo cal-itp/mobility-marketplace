143199ed05473104113241a349f801c184717d0b
Kegan Maher <kegan@compiler.la>
2020-09-03 13:00:21 -0700

    editor component for image with caption
    use figure and figcaption html

--- admin/editor/index.js
+++ admin/editor/index.js
@@ -1,5 +1,7 @@
+import ImageCaption from "/admin/editor/image-caption.js";
 import Youtube from "/admin/editor/youtube.js";
 
+CMS.registerEditorComponent(ImageCaption);
 CMS.registerEditorComponent(Youtube);
 
 console.log("Registered custom editor components");
