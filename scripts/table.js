8a487b2748ed18230628336c59c9dcda404b56ba
Kegan Maher <kegan@compiler.la>
2020-09-16 22:45:05 -0700

    improve column formatting
    * the large text area columns get more width and can go multiline
    * numbers are parsed as numbers first, for accurate sorting
    * format numbers with thousands separator, dollar signs

--- scripts/table.js
+++ scripts/table.js
@@ -2,17 +2,26 @@ $(function() {
   let providers = [];
   const target_id = "#" + data_table.target_id;
 
+  const numberMutator = (value) => value ? parseInt(value) : null;
+
   let table = new Tabulator(target_id, {
     reactiveData: true,
     layout: "fitColumns",
     columns: [
-      {title:"Provider", field:"provider"},
-      {title:"Service County", field:"service_county"},
-      {title:"City", field:"contact_city"},
+      {title:"Provider", field:"provider", formatter:"textarea", widthGrow:2},
+      {title:"Service County", field:"service_county", formatter:"textarea", widthGrow:3},
+      {title:"City", field:"contact_city", formatter:"textarea", widthGrow:2},
       {title:"NTD ID", field:"ntd_id"},
-      {title:"VOMS", field:"voms"},
-      {title:"UPT", field:"upt"},
-      {title:"Fares", field:"fares"},
+      {title:"VOMS", field:"voms", mutator:numberMutator, formatter:"money", formatterParams:{
+        precision:false
+      }},
+      {title:"UPT", field:"upt", mutator:numberMutator, formatter:"money", formatterParams:{
+        precision:false
+      }},
+      {title:"Fares", field:"fares", mutator:numberMutator, formatter:"money", formatterParams:{
+        precision:false,
+        symbol:"$"
+      }},
     ],
     height: "560px",
     pagination:"local"
