d0c47b3731bc4ce2ce4bc96a62e5c5284e107460
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2021-03-09 13:46:11 -0800

    Don't store generated data in git

--- _plugins/csv_to_json.rb
+++ _plugins/csv_to_json.rb
@@ -1,6 +1,6 @@
 Jekyll::Hooks.register :site, :post_write do |_site|
-  Dir.glob(File.join("metadata", "providers", "*.csv")).each do |path|
+  Dir.glob(File.join("_site", "metadata", "providers", "*.csv")).each do |path|
     puts "Formatting CSV to JSON for #{path}"
-    File.write(path.gsub("csv", "json"), JSON.dump(CSV.parse(File.read(path))))
+    File.write(path.gsub("csv", "json"), JSON.dump(CSV.parse(File.read(path), headers: :first_row).map(&:to_h)))
   end
 end
