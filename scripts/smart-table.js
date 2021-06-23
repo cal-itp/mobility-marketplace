70af91a8cb95c7d28104eb73c33b3cb4a0f974f7
Vladimir Jimenez <allejo@me.com>
2021-06-22 23:06:31 -0700

    Fix dupe filters; support multiple filters

--- scripts/smart-table.js
+++ scripts/smart-table.js
@@ -1,7 +1,25 @@
+/*
+   # Smart Tables
+
+   "Smart Tables" are defined as tables with data that can be configured via HTML
+   attributes. See the `contracts/view.html` file as an example. These Smart
+   Tables use the "Tabulator" project and will import HTML tables based on their
+   HTML markup.
+
+   For more information regarding Tabulator's behavior, go here:
+     http://tabulator.info/docs/4.9/data#table
+
+   Here are some notes:
+
+   - A Smart Table is denoted by a `data-role` of `smart-table`
+   - The `data-filterable-cols` takes a comma-separated list of column names (the
+     lowercase variant of the HTML table header. For example, a "Product" column
+     in the table will have a column name of `product`.
+ */
 $(function () {
   const smartTables = document.querySelectorAll('[data-role="smart-table"]');
-  const buildPill = (body) => `
-    <div class="pill">
+  const buildPill = (body, colName) => `
+    <div class="pill" data-column-name="${colName}">
       <span class="sr-only">Filtered by:</span>
       <span class="body">${body}</span>
   
@@ -12,9 +30,13 @@ $(function () {
   `;
 
   smartTables.forEach((tableContainer) => {
-    const filterableCols = tableContainer.getAttribute('data-filterable-cols').split(',');
+    const filterableCols = tableContainer.getAttribute('data-filterable-cols').replace(/[ ]+/g, '').split(',');
     const pillContainer = tableContainer.querySelector('.filterable-table__pills');
     const table = tableContainer.querySelector('table');
+
+    if (filterableCols.length > 0 && pillContainer === null) {
+      console.warn(`Smart Table with ID of #${tableContainer.getAttribute('id')} supports filtering but has no Pill Container.`);
+    }
 
     const tabulator = new Tabulator(table, {
       layout: 'fitColumns',
@@ -22,20 +44,26 @@ $(function () {
         const colDefinition = cell.getColumn().getDefinition();
         const colName = colDefinition['field'];
 
-        if (filterableCols.indexOf(colName) < 0) {
+        if (pillContainer === null || filterableCols.indexOf(colName) < 0) {
           return;
         }
 
         const cellValue = cell.getValue().trim();
 
-        const $pill = $(buildPill(cellValue));
-        $pill.find('.pill__close').on('click', () => {
+        const $pill = $(buildPill(cellValue, colName));
+        $pill.find('.pill__close').on('click', function() {
           pillContainer.removeChild($pill[0]);
-          tabulator.clearFilter();
+          tabulator.removeFilter(colName, "like", cellValue);
         });
 
-        tabulator.setFilter(colName, "like", cellValue);
-        pillContainer.appendChild($pill[0]);
+        /** @type {Array<{field: string, type: string, value: any}>} */
+        const filters = tabulator.getFilters();
+        const filterExists = filters.some(filter => filter.field === colName);
+
+        if (!filterExists) {
+          tabulator.addFilter(colName, "like", cellValue);
+          pillContainer.prepend($pill[0]);
+        }
       },
     });
   });
