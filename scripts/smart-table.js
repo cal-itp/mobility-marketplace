fb9bbe49b115358ec1548e2372a8c137902f7ed3
Vladimir Jimenez <allejo@me.com>
2021-06-22 23:56:08 -0700

    Add support for table filtering from URL params

--- scripts/smart-table.js
+++ scripts/smart-table.js
@@ -15,8 +15,18 @@
    - The `data-filterable-cols` takes a comma-separated list of column names (the
      lowercase variant of the HTML table header. For example, a "Product" column
      in the table will have a column name of `product`.
+   - If the URL has query parameters, then you can automatically filter the table
+     data. Query parameters are in the format of `<table-id>-filter-<column-key>`.
+     For example, to start filtering the "Product" column for the table with an
+     id of `pre-approved-contracts` it'd look for a URL query parameter of
+     `pre-approved-contracts-filter-product`.
  */
 $(function () {
+  function getRandomString() {
+    return Math.random().toString(36).substring(7);
+  }
+
+  const urlParams = new URLSearchParams(window.location.search);
   const smartTables = document.querySelectorAll('[data-role="smart-table"]');
   const buildPill = (body, colName) => `
     <div class="pill" data-column-name="${colName}">
@@ -30,17 +40,60 @@ $(function () {
   `;
 
   smartTables.forEach((tableContainer) => {
+    let tableID = tableContainer.getAttribute('id');
+    if (!tableID) {
+      tableID = getRandomString();
+      tableContainer.setAttribute('id', tableID);
+
+      console.warn(`Smart Table does not have ID, automatically assigning #${tableID}`);
+    }
+
     const filterableCols = tableContainer.getAttribute('data-filterable-cols').replace(/[ ]+/g, '').split(',');
     const pillContainer = tableContainer.querySelector('.filterable-table__pills');
     const table = tableContainer.querySelector('table');
 
     if (filterableCols.length > 0 && pillContainer === null) {
-      console.warn(`Smart Table with ID of #${tableContainer.getAttribute('id')} supports filtering but has no Pill Container.`);
+      console.warn(`Smart Table with ID of #${tableID} supports filtering but has no Pill Container.`);
+      console.warn(tableContainer);
     }
 
-    const tabulator = new Tabulator(table, {
+    const pillFilters = new Set();
+    new Tabulator(table, {
       layout: 'fitColumns',
-      cellClick: (e, cell) => {
+      tableBuilt: function() {
+        const columns = this.getColumnDefinitions();
+
+        for (const column of columns) {
+          const field = column.field;
+          const urlQuery = `${tableID}-filter-${field}`;
+
+          if (urlParams.has(urlQuery)) {
+            this.addFilter(field, 'like', urlParams.get(urlQuery));
+          }
+        }
+      },
+      dataFiltered: function (filters) {
+        for (const filter of filters) {
+          const colName = filter.field;
+          const cellValue = filter.value;
+
+          if (pillFilters.has(cellValue)) {
+            continue;
+          }
+
+          const $pill = $(buildPill(cellValue, colName));
+          $pill.find('.pill__close').on('click', () => {
+            pillContainer.removeChild($pill[0]);
+            pillFilters.delete(cellValue);
+
+            this.removeFilter(colName, 'like', cellValue);
+          });
+
+          pillContainer.prepend($pill[0]);
+          pillFilters.add(cellValue);
+        }
+      },
+      cellClick: function(e, cell) {
         const colDefinition = cell.getColumn().getDefinition();
         const colName = colDefinition['field'];
 
@@ -48,21 +101,13 @@ $(function () {
           return;
         }
 
+        /** @type {Array<{field: string, type: string, value: any}>} */
+        const filters = this.getFilters();
+        const filterExists = filters.some(filter => filter.field === colName);
         const cellValue = cell.getValue().trim();
 
-        const $pill = $(buildPill(cellValue, colName));
-        $pill.find('.pill__close').on('click', function() {
-          pillContainer.removeChild($pill[0]);
-          tabulator.removeFilter(colName, "like", cellValue);
-        });
-
-        /** @type {Array<{field: string, type: string, value: any}>} */
-        const filters = tabulator.getFilters();
-        const filterExists = filters.some(filter => filter.field === colName);
-
         if (!filterExists) {
-          tabulator.addFilter(colName, "like", cellValue);
-          pillContainer.prepend($pill[0]);
+          this.addFilter(colName, 'like', cellValue);
         }
       },
     });
