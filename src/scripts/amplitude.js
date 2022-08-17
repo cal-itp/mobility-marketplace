994a501f45870f833b497fcc6102407a1b137496
Kegan Maher <kegan@compiler.la>
2022-08-17 21:59:56 +0000

    feat(amplitude): helper script with events outline

---
+++ src/scripts/amplitude.js
@@ -0,0 +1,27 @@
+$(function () {
+  const events = {
+    clickedLink: "clicked link",
+    returnedToTop: "returned to top",
+    submittedForm: "submitted form",
+    viewedPage: "viewed page",
+  };
+
+  function path() {
+    // get the full path, including #hash and ?querystring, without the protocol/domain
+    // and remove trailing slash (except for the homepage)
+    // URL: https://www.camobilitymarketplace.org/contracts/view?contracts-filter-product=Transit%20Processor
+    // returns: /contracts/view?contracts-filter-product=Transit%20Processor
+    return window.location.href.replace(window.location.origin, "").replace(/\/$/, "") || "/";
+  }
+
+  function track(event, props = {}) {
+    // track an event with Amplitude
+    const client = amplitude ? amplitude.getInstance() : null;
+
+    if (client) {
+      client.logEvent(event, props);
+    } else {
+      console.warn("Couldn't get Amplitude client for event: [", event, "] with props: ", props);
+    }
+  }
+});
