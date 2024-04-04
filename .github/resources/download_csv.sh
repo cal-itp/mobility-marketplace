6bae7c7b732531e9d9515fca5951959a5ea665b8
Vladimir Jimenez <vlad@compiler.la>
2024-04-03 21:08:22 -0500

    feat: sort provider-map query by NTD ID (#543)

--- .github/resources/download_csv.sh
+++ .github/resources/download_csv.sh
@@ -6,5 +6,5 @@ bq query \
     --format=csv \
     --use_legacy_sql=false \
     --max_rows=500 \
-    "SELECT * FROM \`mart_transit_database.dim_mobility_mart_providers\`" \
+    "SELECT * FROM \`mart_transit_database.dim_mobility_mart_providers\` ORDER BY \`ntd_id\`" \
 > src/metadata/providers/providers.csv
