aca643016071b804e539d13047c94d4f3ff9b013
John Gravois <jgravois@compiler.la>
2026-02-02 19:09:24 +0000

    chore: get 11ty building successfully with JSON data files

---
+++ eleventy.config.js
@@ -0,0 +1,15 @@
+export default async function (eleventyConfig) {
+  eleventyConfig.setInputDirectory("src");
+  eleventyConfig.setLayoutsDirectory("_layouts");
+
+  eleventyConfig.addPassthroughCopy("src/styles/latest.css");
+  eleventyConfig.addPassthroughCopy("src/_redirects");
+
+  // Keeps the same directory structure.
+  eleventyConfig.addPassthroughCopy("**/*.png");
+  eleventyConfig.addPassthroughCopy("**/*.svg");
+
+  eleventyConfig.addPassthroughCopy("src/uploads/**/*.pdf");
+  eleventyConfig.addPassthroughCopy("src/uploads/resources/*.docx");
+  eleventyConfig.addPassthroughCopy("src/uploads/resources/*.zip");
+}
